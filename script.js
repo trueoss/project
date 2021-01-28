8850306bbc60545ff3747b9bbc41c468ec992263
Igor <trueoss1992@gmail.com>
2021-01-28 17:02:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//alert('hello');
+// const result = confirm("are you here?");
+// console.log(result);
+// const answer = +prompt("are you 18?", "18");
+// console.log(answer);
+
+const answers = [];
+
+// answers[0] = prompt("what is you name?", "");
+// answers[1] = prompt("what is you lastname?", "");
+// answers[2] = prompt("how old are you?", "");
+
+// document.write(answers);
+const category = "toys";
+
+console.log(`https://someurl.com/${category}/5`);
+const user = "ivan";
+
+alert(`Hi, ${user}`);
