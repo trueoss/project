a3535e8f476359446f199b71f74bee8bd2f22139
igor <trueoss1992@gmail.com>
2021-02-27 20:33:45 +0700

    added ciclicity

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+'use strict';
 let numberOfFilms = +prompt("How much movie have you watched?","");
 
+    if(numberOfFilms < 10)
+     console.log("Просмотрено довольно мало фильмов");
+      
+    else if(numberOfFilms >= 10 && numberOfFilms < 30)
+    console.log("Вы классический зритель");
+        
+    else if(numberOfFilms > 30) 
+    console.log("Вы киноман");
+        
+    else console.log("Произошла ошибка");
+   
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -7,10 +19,14 @@ let personalMovieDB = {
     genres: [],
     privat: false,
 };
-
+let lastFilm, rate;
+for(let i = 0; i < 2; i ++){
 let lastFilm = prompt("What was the last movie you watched?", "");
 let rate = prompt("how do you rate him?", "");
-personalMovieDB.movies[lastFilm] = rate;
-lastFilm = prompt("What was the last movie you watched?", "");
-rate = prompt("how do you rate him?", "");
-personalMovieDB.movies[lastFilm] = rate;
+while(lastFilm == '' || rate ==''){
+    lastFilm = prompt("What was the last movie you watched?", "");
+    rate = prompt("how do you rate him?", "");
+}personalMovieDB.movies[lastFilm] = rate;
+}
+
+//console.log(personalMovieDB);
