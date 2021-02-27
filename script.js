e079e8bd528be920118924b64d7b888185a48208
igor <trueoss1992@gmail.com>
2021-02-27 18:57:24 +0700

    1

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-//alert('hello');
-// const result = confirm("are you here?");
-// console.log(result);
-// const answer = +prompt("are you 18?", "18");
-// console.log(answer);
+let numberOfFilms = +prompt("How much movie have you watched?","");
 
-const answers = [];
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
-// answers[0] = prompt("what is you name?", "");
-// answers[1] = prompt("what is you lastname?", "");
-// answers[2] = prompt("how old are you?", "");
-
-// document.write(answers);
-const category = "toys";
-
-console.log(`https://someurl.com/${category}/5`);
-const user = "ivan";
-
-alert(`Hi, ${user}`);
+let lastFilm = prompt("What was the last movie you watched?", "");
+let rate = prompt("how do you rate him?", "");
+personalMovieDB.movies[lastFilm] = rate;
+lastFilm = prompt("What was the last movie you watched?", "");
+rate = prompt("how do you rate him?", "");
+personalMovieDB.movies[lastFilm] = rate;
